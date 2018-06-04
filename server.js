fb314ed4fd1b4b4a1772966d843c374f79e8a4c0
Der Meyer <simon.der.meyer@gmail.com>
2018-06-04 19:19:56 +0200

    Prepare deploy to Heroku with package.json and express.

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => res.send({
-    message: `Hi, I'm Noa's Server. Node express proxy on port 5000.`
+    message: `Hi, I'm Noa's Server. Node express proxy on port ${PORT}.`
 }));
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('This is the production build.');
+    app.use(express.static(`${__dirname}/build`));
+    app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
+}
 
 app.listen(PORT, () => console.log(`I'm listening on port ${PORT}`));
