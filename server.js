51aa710ec5f7a4e251f6868dab8dff67b5c2bda1
Der Meyer <simon.der.meyer@gmail.com>
2018-06-04 19:42:47 +0200

    Work both in development via yarn dev and production via Heroku.

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ app.get('/api/hello', (req, res) => res.send({
     message: `Hi, I'm Noa's Server. Node express proxy on port ${PORT}.`
 }));
 
-if (process.env.NODE_ENV === 'production') {
-    console.log('This is the production build.');
-    app.use(express.static(`${__dirname}/client/build`));
-    app.get('*', (req, res) => res.sendFile(`${__dirname}/client/build/index.html`));
-}
+app.use(express.static(`${__dirname}/client/build`));
+
+app.get('*', (req, res) => res.sendFile(`${__dirname}/client/build/index.html`));
 
 app.listen(PORT, () => console.log(`I'm listening on port ${PORT}`));
