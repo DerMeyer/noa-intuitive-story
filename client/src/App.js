2d45c02ac01df51f5ac505e48a07aef5db2b9fc0
Der Meyer <simon.der.meyer@gmail.com>
2018-06-08 20:17:55 +0200

    Add redux and react browser router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 import Navigation from './navigation';
@@ -6,27 +7,25 @@ import Timeline from './timeline';
 import Footer from './footer';
 import Architypes from './architypes';
 
-const sledWidth = 253;
+const sled_width = 253;
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            server_message: ''
+            server_message: '',
+            timeline_left: `-${(sled_width - 100) / 2}vw`
         }
     }
     componentDidMount() {
-
         this.serverSaysHi();
-
-        const sled = document.getElementById('timeline_sled');
         window.addEventListener(
             'scroll',
-            () => sled.style.left = `-${window.scrollY * (window.innerWidth * ((sledWidth - 100) / 200)) / window.innerHeight}px`
+            () => this.setState({
+                timeline_left: `-${window.scrollY * (window.innerWidth * ((sled_width - 100) / 200)) / window.innerHeight}px`
+            })
         );
-        setTimeout(() => {
-            window.scroll(0, window.innerHeight);
-        }, 50);
+        window.scroll(0, window.innerHeight);
     }
     serverSaysHi = async () => {
         try {
@@ -43,20 +42,29 @@ class App extends Component {
         }
     }
     render() {
+        const { timeline_left } = this.state;
+
+        const sledStyle = {
+            width: `${sled_width}vw`,
+            left: timeline_left
+        }
+
         return (
-            <main>
-                <Navigation />
-                <section id="timeline_container">
-                    <section id="timeline_sled" style={{width: `${sledWidth}vw`}}>
-                        <Timeline />
+            <BrowserRouter>
+                <main>
+                    <Navigation />
+                    <section id="timeline_container">
+                        <section id="timeline_sled" style={sledStyle}>
+                            <Route exact path="/" component={Timeline} />
+                            <Route path="/arch" component={Architypes} />
+                        </section>
                     </section>
-                </section>
-                <Footer />
-                <Architypes />
-                <p className="server_greeting">
-                    {this.state.server_message}
-                </p>
-            </main>
+                    <Footer />
+                    <p className="server_greeting">
+                        {this.state.server_message}
+                    </p>
+                </main>
+            </BrowserRouter>
         );
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
 import './index.css';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, composeWithDevTools());
+
+const main = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(main, document.getElementById('root'));
