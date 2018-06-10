3759a7d69dfc9b9c5ec48408e8b4e044831f3558
Der Meyer <simon.der.meyer@gmail.com>
2018-06-10 12:45:41 +0200

    Grab and drag timeline with mouse, connect timeline with redux.

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,15 @@ import Timeline from './timeline';
 import Footer from './footer';
 import Architypes from './architypes';
 
-const sled_width = 253;
-
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            server_message: '',
-            timeline_left: `-${(sled_width - 100) / 2}vw`
+            server_message: ''
         }
     }
     componentDidMount() {
         this.serverSaysHi();
-        window.addEventListener(
-            'scroll',
-            () => this.setState({
-                timeline_left: `-${window.scrollY * (window.innerWidth * ((sled_width - 100) / 200)) / window.innerHeight}px`
-            })
-        );
-        window.scroll(0, window.innerHeight);
     }
     serverSaysHi = async () => {
         try {
@@ -42,23 +32,12 @@ class App extends Component {
         }
     }
     render() {
-        const { timeline_left } = this.state;
-
-        const sledStyle = {
-            width: `${sled_width}vw`,
-            left: timeline_left
-        }
-
         return (
             <BrowserRouter>
                 <main>
                     <Navigation />
-                    <section id="timeline_container">
-                        <section id="timeline_sled" style={sledStyle}>
-                            <Route exact path="/" component={Timeline} />
-                            <Route path="/arch" component={Architypes} />
-                        </section>
-                    </section>
+                    <Route exact path="/" component={Timeline} />
+                    <Route path="/arch" component={Architypes} />
                     <Footer />
                     <p className="server_greeting">
                         {this.state.server_message}
--- client/src/timeline.js
+++ client/src/timeline.js
@@ -1,24 +1,84 @@
-import React, { Component } from "react";
-import "./timeline.css";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+let timelineSled;
+
+const SLED_WIDTH = 253;
 
 class Timeline extends Component {
+    state = {
+        hasCapture: false,
+        timelineLeft: `-${(SLED_WIDTH - 100) / 2}vw`
+    }
+    isDragging = false;
+    previousLeft = 0;
+    componentDidMount() {
+        timelineSled = document.getElementById('timeline_sled');
+        window.addEventListener('scroll', () => this.setTimelinePosition());
+        window.scroll(0, window.innerHeight);
+    }
+    onDown = event => {
+        this.isDragging = true;
+        event.target.setPointerCapture(event.pointerId);
+        this.extractLeftDelta(event);
+    };
+    onMove = event => {
+        event.preventDefault();
+        if (!this.isDragging) {
+            return;
+        }
+        const left = this.extractLeftDelta(event);
+        window.scroll(0, (- timelineSled.offsetLeft - left) / (window.innerWidth * ((SLED_WIDTH - 100) / 200)) / window.innerHeight);
+    };
+    onUp = event => this.isDragging = false;
+    onGotCapture = event => this.setState({ hasCapture: true });
+    onLostCapture = event =>
+        this.setState({ hasCapture: false });
+    extractLeftDelta = event => {
+        const left = event.pageX;
+        const delta = left - this.previousLeft;
+        this.previousLeft = left;
+        return delta;
+    };
+    setTimelinePosition = () => this.setState({
+        timelineLeft: `-${window.scrollY * (window.innerWidth * ((SLED_WIDTH - 100) / 200)) / window.innerHeight}px`
+    })
     render() {
+        const { timelineLeft } = this.state;
+        const style = {
+            timelineSled: {
+                position: 'absolute',
+                left: timelineLeft,
+                top: '13vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '82vh',
+                width: `${SLED_WIDTH}vw`
+            },
+            timeline: {
+                width: '260vw'
+            }
+        }
         return (
-            <section className="timeline_frame">
-                <img src="/images/timeline.jpg" alt="Timeline" id="timeline"/>
+            <section
+                id="timeline_sled"
+                style={style.timelineSled}
+                onPointerDown={this.onDown}
+                onPointerMove={this.onMove}
+                onPointerUp={this.onUp}
+                onPointerCancel={this.onUp}
+                onGotPointerCapture={this.onGotCapture}
+                onLostPointerCapture={this.onLostCapture}
+                >
+                <img style={style.timeline} src="/images/timeline.jpg" alt="Timeline" id="timeline"/>
             </section>
         );
     }
 }
 
-export default Timeline;
+const mapStateToProps = state => state;
 
-// <section id="scroll_container" ref={elem => this.elem = elem} onScroll={() => console.log(this.elem.scrollTop)}>
-//     <section className="scroll_sled">
-//         <section className="timeline_container">
-//             <section id="timeline_sled">
-//                 <img src="/images/timeline.jpg" alt="Timeline" id="timeline"/>
-//             </section>
-//         </section>
-//     </section>
-// </section>
+const ConnectedTimeline = connect(mapStateToProps)(Timeline);
+
+export default ConnectedTimeline;
