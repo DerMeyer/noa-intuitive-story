dc84bc110cd33b7541b823f5f2df00cb0d264f00
Der Meyer <simon.der.meyer@gmail.com>
2018-06-10 19:56:16 +0200

    Add mapPixelToTimeline method to timeline component.

--- client/src/timeline.js
+++ client/src/timeline.js
@@ -2,6 +2,80 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 const SLED_WIDTH = 253;
+const TIMELINE_MAP = [
+    [0, -80000],
+    [45, -70000],
+    [90, -60000],
+    [133, -50000],
+    [178, -40000],
+    [222, -30000],
+    [267, -20000],
+    [310, -10000],
+    [343, -9000],
+    [376, -8000],
+    [410, -7000],
+    [443, -6000],
+    [477, -5000],
+    [510, -4800],
+    [543, -4600],
+    [576, -4400],
+    [609, -4200],
+    [642, -4000],
+    [675, -3800],
+    [708, -3600],
+    [741, -3400],
+    [775, -3200],
+    [807, -3000],
+    [840, -2800],
+    [873, -2600],
+    [906, -2400],
+    [939, -2200],
+    [972, -2000],
+    [1005, -1800],
+    [1038, -1600],
+    [1072, -1400],
+    [1105, -1200],
+    [1138, -1000],
+    [1171, -900],
+    [1204, -800],
+    [1237, -700],
+    [1270, -600],
+    [1304, -500],
+    [1336, -400],
+    [1369, -300],
+    [1403, -200],
+    [1436, -100],
+    [1469, 0],
+    [1520, 100],
+    [1576, 200],
+    [1632, 300],
+    [1687, 400],
+    [1743, 500],
+    [1802, 600],
+    [1858, 700],
+    [1914, 800],
+    [1968, 900],
+    [2024, 1000],
+    [2078, 1100],
+    [2134, 1200],
+    [2189, 1300],
+    [2245, 1400],
+    [2302, 1500],
+    [2360, 1600],
+    [2416, 1700],
+    [2472, 1800],
+    [2528, 1900],
+    [2583, 1950],
+    [2639, 2000],
+    [2672, 2100],
+    [2706, 2200],
+    [2739, 2300],
+    [2773, 2400],
+    [2806, 2500],
+    [2873, 3000],
+    [2958, 4000],
+    [2980, 5000]
+]
 
 class Timeline extends Component {
     state = {
@@ -47,6 +121,19 @@ class Timeline extends Component {
         this.previousLeft = left;
         return delta;
     };
+    mapPixelToTimeline = event => {
+        const currentTimelineScale = 2980 / document.getElementById('timeline_sled').offsetWidth;
+        const timelineX = (event.pageX - document.getElementById('timeline_sled').offsetLeft) * currentTimelineScale;
+        let timelineSection;
+        TIMELINE_MAP.forEach((v, i, a) => {
+            if (a[i + 1]) {
+                if (v[0] < timelineX && a[i + 1][0] > timelineX) {
+                    timelineSection = [ v, a[i + 1] ];
+                }
+            }
+        });
+        return Math.floor(timelineSection[0][1] + ((timelineSection[1][1] - timelineSection[0][1]) * ((timelineSection[0][0] - timelineX) / (timelineSection[0][0] - timelineSection[1][0]))));
+    }
     render() {
         const { timelineLeft } = this.state;
         const style = {
@@ -74,6 +161,7 @@ class Timeline extends Component {
                 onPointerCancel={this.onUp}
                 onGotPointerCapture={this.onGotCapture}
                 onLostPointerCapture={this.onLostCapture}
+                onClick={this.mapPixelToTimeline}
                 >
                 <img id="timeline" style={style.timeline} src="/images/timeline.jpg" alt="Timeline" />
             </section>
