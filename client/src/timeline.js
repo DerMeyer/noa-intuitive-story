6f19bfa42640542108b622650284e8d8b64d7dd9
Der Meyer <simon.der.meyer@gmail.com>
2018-06-10 16:05:29 +0200

    Improve timeline scroll.

--- client/src/timeline.js
+++ client/src/timeline.js
@@ -5,20 +5,24 @@ const SLED_WIDTH = 253;
 
 class Timeline extends Component {
     state = {
-        hasCapture: false,
-        timelineLeft: `-${(SLED_WIDTH - 100) / 2}vw`
+            hasCapture: false,
+            timelineLeft: window.innerWidth * (SLED_WIDTH - 100) / 200
     }
     isDragging = false;
     previousLeft = 0;
     scrollFactor = window.innerWidth / window.innerHeight * ((SLED_WIDTH - 100) / 200);
     componentDidMount() {
-        window.addEventListener('scroll', () => this.setState({
-            timelineLeft: `-${window.scrollY * this.scrollFactor}px`
-        }));
         window.scroll(0, window.innerHeight);
+        window.addEventListener('scroll', this.setTimelineLeft);
+    }
+    setTimelineLeft = () => {
+        this.setState({
+            timelineLeft: window.scrollY * this.scrollFactor
+        })
     }
     onDown = event => {
         event.target.setPointerCapture(event.pointerId);
+        window.removeEventListener('scroll', this.setTimelineLeft);
         this.isDragging = true;
         this.scrollFactor = window.innerWidth / window.innerHeight * ((SLED_WIDTH - 100) / 200);
         this.extractLeftDelta(event);
@@ -29,9 +33,15 @@ class Timeline extends Component {
             return;
         }
         const left = this.extractLeftDelta(event);
-        window.scroll(0, (- document.getElementById('timeline_sled').offsetLeft - left) / this.scrollFactor);
+        this.setState(({ timelineLeft }) => ({
+            timelineLeft: timelineLeft - left
+        }));
     };
-    onUp = event => this.isDragging = false;
+    onUp = event => {
+        window.scroll(0, - document.getElementById('timeline_sled').offsetLeft / this.scrollFactor);
+        window.addEventListener('scroll', this.setTimelineLeft);
+        this.isDragging = false;
+    }
     onGotCapture = event => this.setState({ hasCapture: true });
     onLostCapture = event => this.setState({ hasCapture: false });
     extractLeftDelta = event => {
@@ -45,13 +55,14 @@ class Timeline extends Component {
         const style = {
             timelineSled: {
                 position: 'absolute',
-                left: timelineLeft,
+                left: `-${timelineLeft}px`,
                 top: '13vh',
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
                 height: '82vh',
-                width: `${SLED_WIDTH}vw`
+                width: `${SLED_WIDTH}vw`,
+                transform: this.hasCapture ? 'skew(.5)' : 'skew(1)'
             },
             timeline: {
                 width: '260vw'
