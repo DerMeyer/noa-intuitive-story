2fab2745f418852f11b57731c7cc72426b7c1528
Der Meyer <simon.der.meyer@gmail.com>
2018-06-28 13:02:40 +0200

    Update.

---
+++ sql/intuitive_notes.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS intuitive_notes;
+
+CREATE TABLE intuitive_notes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    img_url VARCHAR(128),
+    headline TEXT,
+    body TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
