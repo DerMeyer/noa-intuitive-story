f5dc9fd2c5ba202c9c010a99d22f6008194f1db1
Der Meyer <simon.der.meyer@gmail.com>
2018-10-03 00:14:43 +0200

    Factor out ProfileNavigation from App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, { PureComponent } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
+import React from 'react';
 import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom';
 
+import './App.css';
+
+import ProfileNavigation from './ProfileNavigation';
 import Timeline from './Timeline';
 import About from './About';
 import Groups from './Groups';
@@ -13,132 +14,60 @@ import Admin from './Admin';
 import ProfilePage from './ProfilePage';
 import GroupPage from './GroupPage';
 
-import { checkSignIn, signOut } from './actions';
-
-class App extends PureComponent {
-    componentDidMount() {
-        this.props.dispatch(checkSignIn());
-    }
-
-    signOut = () => {
-        this.props.dispatch(signOut());
-        window.location.replace('/');
-    };
-
-    render() {
-        return (
-            <BrowserRouter>
-                <main>
-                    <header className="header flex">
-                        <nav className="header__nav inline-flex">
-                            <img
-                                className="header__nav__image"
-                                title="home"
-                                src="favicon.png"
-                                alt="Logo"
-                            />
-                            <NavLink
-                                exact
-                                to="/"
-                                className="header__nav__button"
-                            >
-                                Timeline
-                            </NavLink>
-                            <NavLink
-                                to="/about"
-                                className="header__nav__button"
-                            >
-                                About the Intuitive Story
-                            </NavLink>
-                            <NavLink
-                                to="/groups"
-                                className="header__nav__button"
-                            >
-                                Group Collection
-                            </NavLink>
-                        </nav>
-                        {this.props.signedIn ? (
-                            <nav className="profile__nav inline-flex">
-                                <Link
-                                    to="/"
-                                    className="profile__nav__button"
-                                    onClick={this.signOut}
-                                >
-                                    Sign out
-                                </Link>
-                                <Link
-                                    to={`/profile/${this.props.user.alias}`}
-                                    className="profile__nav__button"
-                                >
-                                    <div
-                                        style={{
-                                            backgroundImage: `url(${this.props.user.icon_url || 'favicon.png'})`
-                                        }}
-                                        className="profile__nav__image"
-                                        title="view your profile"
-                                    />
-                                </Link>
-                            </nav>
-                        ) : (
-                            <nav className="profile__nav inline-flex">
-                                <Link
-                                    to="/signin"
-                                    className="profile__nav__button"
-                                >
-                                    Sign in
-                                </Link>
-                                <Link
-                                    to="/signup"
-                                    className="profile__nav__button profile__nav__button--border"
-                                >
-                                    Get started
-                                </Link>
-                            </nav>
-                        )}
-                    </header>
-
-                    <Route exact path="/" component={Timeline} />
-                    <Route path="/about" component={About} />
-                    <Route path="/groups" component={Groups} />
-                    <Route path="/signin" component={SignIn} />
-                    <Route path="/signup" component={SignUp} />
-                    <Route path="/verify" component={Verify} />
-                    <Route path="/avira" component={Admin} />
-                    <Route
-                        path="/profile/:user"
-                        render={props => <ProfilePage match={props.match} />}
+const App = () => (
+    <BrowserRouter>
+        <main>
+            <header className="header flex">
+                <nav className="header__nav inline-flex">
+                    <img
+                        className="header__nav__image"
+                        title="home"
+                        src="favicon.png"
+                        alt="Logo"
                     />
-                    <Route
-                        path="/group/:id"
-                        render={props => <GroupPage match={props.match} />}
-                    />
+                    <NavLink exact to="/" className="header__nav__button">
+                        Timeline
+                    </NavLink>
+                    <NavLink to="/about" className="header__nav__button">
+                        About the Intuitive Story
+                    </NavLink>
+                    <NavLink to="/groups" className="header__nav__button">
+                        Group Collection
+                    </NavLink>
+                </nav>
 
-                    <footer className="footer flex">
-                        <span className="footer__note">&copy; Noa Golan</span>
-                        <nav className="footer__nav inline-flex">
-                            <Link
-                                to="/impressum"
-                                className="footer__nav__button"
-                            >
-                                Impressum
-                            </Link>
-                            <Link to="/contact" className="footer__nav__button">
-                                Contact
-                            </Link>
-                        </nav>
-                    </footer>
-                </main>
-            </BrowserRouter>
-        );
-    }
-}
+                <ProfileNavigation />
+            </header>
 
-const mapStateToProps = ({ signedIn, message = {}, user = {} }) => ({
-    signedIn,
-    message,
-    user
-});
+            <Route exact path="/" component={Timeline} />
+            <Route path="/about" component={About} />
+            <Route path="/groups" component={Groups} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/verify" component={Verify} />
+            <Route path="/avira" component={Admin} />
+            <Route
+                path="/profile/:user"
+                render={props => <ProfilePage match={props.match} />}
+            />
+            <Route
+                path="/group/:id"
+                render={props => <GroupPage match={props.match} />}
+            />
 
-const ConnectedApp = connect(mapStateToProps)(App);
+            <footer className="footer flex">
+                <span className="footer__note">&copy; Noa Golan</span>
+                <nav className="footer__nav inline-flex">
+                    <Link to="/impressum" className="footer__nav__button">
+                        Impressum
+                    </Link>
+                    <Link to="/contact" className="footer__nav__button">
+                        Contact
+                    </Link>
+                </nav>
+            </footer>
+        </main>
+    </BrowserRouter>
+);
 
-export default ConnectedApp;
+export default App;
---
+++ client/src/ProfileNavigation.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { checkSignIn, signOut } from './actions';
+
+class ProfileNavigation extends Component {
+    componentDidMount() {
+        this.props.dispatch(checkSignIn());
+    }
+
+    signOut = () => {
+        this.props.dispatch(signOut());
+        window.location.replace('/');
+    }
+
+    render() {
+        if (this.props.signedIn) {
+            return (
+                <nav className="profile__nav inline-flex">
+                    <Link
+                        to="/"
+                        className="profile__nav__button"
+                        onClick={this.signOut}
+                    >
+                        Sign out
+                    </Link>
+                    <Link
+                        to={`/profile/${this.props.user.alias}`}
+                        className="profile__nav__button"
+                    >
+                        <div
+                            style={{
+                                backgroundImage: `url(${this.props.user.icon_url || 'favicon.png'})`
+                            }}
+                            className="profile__nav__image"
+                            title="view your profile"
+                        />
+                    </Link>
+                </nav>
+            )
+        }
+        return (
+            <nav className="profile__nav inline-flex">
+                <Link
+                    to="/signin"
+                    className="profile__nav__button"
+                >
+                    Sign in
+                </Link>
+                <Link
+                    to="/signup"
+                    className="profile__nav__button profile__nav__button--border"
+                >
+                    Get started
+                </Link>
+            </nav>
+        )
+    }
+}
+
+const mapStateToProps = ({ signedIn, user = {} }) => ({
+    signedIn,
+    user
+});
+
+const ConnectedProfileNavigation = connect(mapStateToProps)(ProfileNavigation);
+
+export default ConnectedProfileNavigation;
